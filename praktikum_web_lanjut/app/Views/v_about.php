06d96c18fd78e120afc9a59ebe715e60f260576c
nandafajar2019 <nandafajar2019@gmail.com>
2021-09-16 21:09:39 +0700

    Praktikum Web Lanjut Tugas1

---
+++ praktikum_web_lanjut/app/Views/v_about.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="text-center">
+        <img src="/assets/images/FC_Barcelona_(crest).svg.png" alt="logo" width="180" class="img-thumbnail">
+        <h1 class="display-4">Praktikum Pemrograman Web Lanjut dengan Studi Kasus Blog App</h1>
+        <p class="lead">Programming | Codeigniter 4 | Php</p>
+    </div>
+</div>   
+
+    
---
+++ praktikum_web_lanjut/app/Views/v_home.php
@@ -0,0 +1,8 @@
+<div class="container mt-3">
+    <div class="text-center">
+        <h1 class="display-4">Selamat Datang di Kelas Praktikum Pemrograman Web Lanjut</h1>
+    </div>
+</div>   
+
+    
+   
