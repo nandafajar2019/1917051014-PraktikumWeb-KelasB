ef4b288d422913844edf475daa1a9c59abc83341
nandafajar2019 <nandafajar2019@gmail.com>
2021-09-23 21:36:35 +0700

    Praktikum web lanjut

--- praktikum_web_lanjut/app/Views/v_posts.php
+++ praktikum_web_lanjut/app/Views/v_posts.php
@@ -2,4 +2,89 @@
     <div class="text-center">
         <h1>All Posts</h1>
     </div>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <form action="" method="GET">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Cari postingan" aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?nature" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Nature</h5>
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?sport" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Sport</h5>
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?food" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Food</h5>
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?lifestyle" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Lifestyle</h5>
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?programming" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Programming</h5>
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </div>
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <img src="https://source.unsplash.com/1600x900/?gaming" class="card-img-top" alt="gambar">
+                <div class="card-body">
+                    <a href="" class="text-decoration-none text-dark">
+                        <h5 class="card-title">Gaming</h5>
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </div>
+    </div>
+    
 </div>   
