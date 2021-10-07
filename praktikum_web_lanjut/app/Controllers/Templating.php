5703c111f94233e76055f60dffc0c917517d02ef
nandafajar2019 <nandafajar2019@gmail.com>
2021-10-07 21:04:08 +0700

    Praktikum web lanjut

--- praktikum_web_lanjut/app/Controllers/Templating.php
+++ praktikum_web_lanjut/app/Controllers/Templating.php
@@ -3,19 +3,48 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class Templating extends BaseController
 {
+    public function __construct()
+    {
+         $this->userModel = new UserModel();
+    }
+
     public function index()
     {
         $data = [
-            'title' => "Blog - Posts",
+            'title' => "Admin",
             
         ];
         // echo view('layouts/header', $data);
         // echo view('layouts/navbar');
         // echo view('v_posts');
         // echo view('layouts/footer');
-        return view('view_admin');  
+        return view('view_admin', $data);  
+    }
+    public function register()
+    {
+        $data = [
+            'title' => "Register",
+            
+        ];
+        // echo view('layouts/header', $data);
+        // echo view('layouts/navbar');
+        // echo view('v_posts');
+        // echo view('layouts/footer');
+        return view('view_register', $data);  
+    }
+    public function saveregister()
+    {
+        $request = service('request');
+        $data = [
+            'fullname' => $request->getVar('fullname'),
+            'email' => $request->getVar('email'),
+            'password' => $request->getVar('password'),
+        ];
+        $this->userModel->insert($data);
+        return redirect()->to(base_url('register'));
     }
 }
